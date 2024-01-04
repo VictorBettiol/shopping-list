2569563ca6306818b580bb42c02bb95558fc9089
Victor Bettiol <victor.bettiol@hotmail.com>
2024-01-04 22:43:31 +0000

    feat: add checkUI for checking the state of the list and filterItems

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
 const clearBtn = document.getElementById('clear')
+const itemFilter = document.getElementById('filter')
 
 function addItem(e) {
   e.preventDefault()
@@ -21,7 +22,10 @@ function addItem(e) {
   const button = createButton('remove-item btn-link text-red')
   li.appendChild(button)
 
+  // Add li to the DOM
   itemList.appendChild(li)
+
+  checkUI()
 
   itemInput.value = ''
 }
@@ -29,6 +33,7 @@ function addItem(e) {
 function createButton(classes) {
   const button = document.createElement('button')
   button.className = classes
+
   const icon = createIcon('fa-solid fa-xmark')
   button.appendChild(icon)
   return button
@@ -36,19 +41,53 @@ function createButton(classes) {
 
 function createIcon(classes) {
   const icon = document.createElement('i')
+
   icon.className = classes
   return icon
 }
 
 function removeItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove()
+    if (window.confirm('Are you sure you want to delete the item?')) {
+      e.target.parentElement.parentElement.remove()
+      checkUI()
+    }
   }
 }
 
 function clearItems() {
-  while (itemList.firstChild) {
-    itemList.removeChild(itemList.firstChild)
+  if (window.confirm('Are you sure you want to delete all of the items?')) {
+    while (itemList.firstChild) {
+      itemList.removeChild(itemList.firstChild)
+    }
+    checkUI()
+  }
+}
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll('li')
+  const text = e.target.value.toLowerCase()
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase()
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex'
+    } else {
+      item.style.display = 'none'
+    }
+  })
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll('li')
+
+  if (items.length === 0) {
+    clearBtn.style.display = 'none'
+    itemFilter.style.display = 'none'
+  } else {
+    clearBtn.style.display = 'block'
+    itemFilter.style.display = 'block'
   }
 }
 
@@ -56,3 +95,6 @@ function clearItems() {
 itemForm.addEventListener('submit', addItem)
 itemList.addEventListener('click', removeItem)
 clearBtn.addEventListener('click', clearItems)
+itemFilter.addEventListener('input', filterItems)
+
+checkUI()
